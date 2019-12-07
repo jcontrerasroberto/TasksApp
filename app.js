d18e1badc5e675b07d36f474f8c5891c4b8c74cf
José Roberto Contreras Barrita <contreras.jrobert@gmail.com>
2019-12-06 22:57:19 -0600

    Interface and way to display tasks chenged

--- app.js
+++ app.js
@@ -32,48 +32,59 @@ function getTasks() {
 function insertTaskUI(tasks){
   let tasksview = document.getElementById('tasks');
   tasksview.innerHTML = '';
-  for (var i = 0; i < tasks.length; i++) {
+  tasksview.innerHTML+="<h3>Important</h3>";
+  for (var i = tasks.length-1; i >= 0; i--) {
     let title= tasks[i].title;
     let description= tasks[i].description;
     let priority = tasks[i].priority;
     if(priority==="High"){
       tasksview.innerHTML +=
-      `<div class="card mb-3">
+      `<div class="card mb-3 col-md-5 d-inline-block mr-2 border-warning">
         <div class="card-body">
           <h4><strong>${title}</strong></h4><h5>${description}</h5>
-          <a class="btn btn-danger" onclick="deleteTask('${title}')">Delete</a>
+          <a class="btn btn-danger mt-4 btn-block" onclick="deleteTask('${title}')">Delete</a>
         </div>
       </div>`;
     }
   }
-  for (var i = 0; i < tasks.length; i++) {
+
+  tasksview.innerHTML+="<br>";
+  tasksview.innerHTML+="<h3>More or less important</h3>";
+
+  for (var i = tasks.length-1; i >= 0; i--) {
     let title= tasks[i].title;
     let description= tasks[i].description;
     let priority = tasks[i].priority;
     if(priority==="Medium"){
       tasksview.innerHTML +=
-      `<div class="card mb-3">
+      `<div class="card mb-3 col-md-5 d-inline-block mr-2 border-warning">
         <div class="card-body">
           <h4><strong>${title}</strong></h4><h5>${description}</h5>
-          <a class="btn btn-danger" onclick="deleteTask('${title}')">Delete</a>
+          <a class="btn btn-danger mt-4 btn-block" onclick="deleteTask('${title}')">Delete</a>
         </div>
       </div>`;
     }
   }
-  for (var i = 0; i < tasks.length; i++) {
+
+  tasksview.innerHTML+="<br>";
+  tasksview.innerHTML+="<h3>Not that important but do it</h3>";
+
+  for (var i = tasks.length-1; i >= 0; i--) {
     let title= tasks[i].title;
     let description= tasks[i].description;
     let priority = tasks[i].priority;
     if(priority==="Low"){
       tasksview.innerHTML +=
-      `<div class="card mb-3">
+      `<div class="card mb-3 col-md-5 d-inline-block mr-1 border-warning">
         <div class="card-body">
           <h4><strong>${title}</strong></h4><h5>${description}</h5>
-          <a class="btn btn-danger" onclick="deleteTask('${title}')">Delete</a>
+          <a class="btn btn-danger mt-4 btn-block" onclick="deleteTask('${title}')">Delete</a>
         </div>
       </div>`;
     }
   }
+
+  tasksview.innerHTML+="<br><br>";
 }
 
 function deleteTask(title){
